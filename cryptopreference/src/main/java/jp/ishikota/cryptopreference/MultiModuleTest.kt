8a45a86f26e546da17c266260ef3659c14ef9dc2
kota-ishimoto <kota-ishimoto@cookpad.com>
2018-04-01 20:02:33 +0900

    Add library module

---
+++ cryptopreference/src/main/java/jp/ishikota/cryptopreference/MultiModuleTest.kt
@@ -0,0 +1,11 @@
+package jp.ishikota.cryptopreference
+
+import android.util.Log
+
+class MultiModuleTest {
+    companion object {
+        fun test() {
+            Log.d("TEST", "MultiModuleTest success!!")
+        }
+    }
+}
