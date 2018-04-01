d82c1f151d39de82b6a9f838b0ca5e9e26103323
kota-ishimoto <kota-ishimoto@cookpad.com>
2018-04-01 20:03:02 +0900

    Remove test code

--- sample/src/main/java/jp/ishikota/cryptopreference/sample/MainActivity.kt
+++ sample/src/main/java/jp/ishikota/cryptopreference/sample/MainActivity.kt
@@ -2,13 +2,11 @@ package jp.ishikota.cryptopreference.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import jp.ishikota.cryptopreference.MultiModuleTest
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        MultiModuleTest.test()
     }
 }
