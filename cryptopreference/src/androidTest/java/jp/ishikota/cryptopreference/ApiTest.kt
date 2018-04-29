625ed88684c96b5e752ef8eea5486ae613b1555c
kota-ishimoto <kota-ishimoto@cookpad.com>
2018-04-29 23:49:27 +0900

    Update api

---
+++ cryptopreference/src/androidTest/java/jp/ishikota/cryptopreference/ApiTest.kt
@@ -0,0 +1,43 @@
+package jp.ishikota.cryptopreference
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Test
+
+class ApiTest {
+
+    private val preferenceName = "CryptoPreference"
+
+    @After
+    fun tearDown() {
+        cleanupPreference()
+    }
+
+    @Test(expected = InitializationException::class)
+    fun callCreateWithoutInitialization() {
+        val context = InstrumentationRegistry.getTargetContext()
+        CryptoPreference.create(context, Algorithm.AES, BlockMode.CBC, Padding.PKCS7)
+    }
+
+    @Test
+    fun callCreate() {
+        val context = InstrumentationRegistry.getTargetContext()
+
+        CryptoPreference.Initializer.also {
+            it.preferenceName = preferenceName
+            it.secretKeyForCompat = "1234567890123456".toByteArray()
+        }
+        val encryptedPref = CryptoPreference.create(context, Algorithm.AES, BlockMode.CBC, Padding.PKCS7)
+        encryptedPref.savePrivateString("HOGE", "FUGA")
+
+        assertEquals("FUGA", encryptedPref.getPrivateString("HOGE"))
+    }
+
+    private fun cleanupPreference() {
+        val context = InstrumentationRegistry.getTargetContext()
+        val pref = context.getSharedPreferences(preferenceName, Context.MODE_PRIVATE)
+        pref.all.keys.forEach { pref.edit().remove(it).apply() }
+    }
+}
--- cryptopreference/src/main/java/jp/ishikota/cryptopreference/Api.kt
+++ cryptopreference/src/main/java/jp/ishikota/cryptopreference/Api.kt
@@ -1,5 +1,60 @@
 package jp.ishikota.cryptopreference
 
+import android.content.Context
+import jp.ishikota.cryptopreference.keycontainer.SecretKeyContainerFactory
+import jp.ishikota.cryptopreference.preference.encrypted.EncryptedPreference
+import jp.ishikota.cryptopreference.preference.encrypted.EncryptedPreferenceFactory
+import jp.ishikota.cryptopreference.preference.plain.PlainPreferenceFactory
+
+class CryptoPreference {
+
+    companion object {
+
+        private val encryptedPreferenceFactory = EncryptedPreferenceFactory()
+
+        fun create(context: Context,
+            algorithm: Algorithm,
+            blockMode: BlockMode,
+            padding: Padding): EncryptedPreference {
+
+            if (!Initializer.isInitialized()) {
+                throw InitializationException()
+            }
+
+            val plainPreferenceFactory = PlainPreferenceFactory(Initializer.preferenceName!!)
+            val secretKeyContainer = SecretKeyContainerFactory(Initializer.secretKeyForCompat).create()
+            val plainPreference = plainPreferenceFactory.create(context, Initializer.debugMode)
+            return encryptedPreferenceFactory.create(
+                secretKeyContainer,
+                plainPreference,
+                algorithm,
+                blockMode,
+                padding
+            )
+        }
+
+    }
+
+    class Initializer {
+
+        companion object {
+
+            var secretKeyForCompat: ByteArray? = null
+
+            var preferenceName: String? = null
+
+            var debugMode: Boolean = false
+
+            internal fun isInitialized() =
+                secretKeyForCompat != null &&
+                    preferenceName != null
+        }
+
+    }
+}
+
+class InitializationException: RuntimeException()
+
 enum class Algorithm(val label: String) {
     AES("AES")
 }
