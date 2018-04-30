d8578b458d046207c372f898ad065d5ddd1f3e09
kota-ishimoto <kota-ishimoto@cookpad.com>
2018-04-30 19:09:28 +0900

    Introduce stetho

--- sample/src/main/java/jp/ishikota/cryptopreference/sample/ui/CryptoPreferenceSampleApp.kt
+++ sample/src/main/java/jp/ishikota/cryptopreference/sample/ui/CryptoPreferenceSampleApp.kt
@@ -1,16 +1,22 @@
 package jp.ishikota.cryptopreference.sample.ui
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import jp.ishikota.cryptopreference.CryptoPreference
 
 class CryptoPreferenceSampleApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        // setup for  CryptoPreference
         CryptoPreference.Initializer.apply {
             secretKeyForCompat = byteArrayOf()
             preferenceName = CRYPTO_PREFERENCE_NAME
         }
+
+        // setup for Stetho
+        Stetho.initializeWithDefaults(this)
     }
 
     companion object {
